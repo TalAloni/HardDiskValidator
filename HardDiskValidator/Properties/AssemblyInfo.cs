cda356fffd7fc295fef0583cb01c703c93c2701f
Tal Aloni <tal.aloni.il@gmail.com>
2019-10-31 22:50:40 +0200

    Hard Disk Validator v1.0.9

--- HardDiskValidator/Properties/AssemblyInfo.cs
+++ HardDiskValidator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tal Aloni")]
 [assembly: AssemblyProduct("Hard Disk Validator")]
-[assembly: AssemblyCopyright("Copyright © Tal Aloni 2016-2018")]
+[assembly: AssemblyCopyright("Copyright © Tal Aloni 2016-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.8.0")]
-[assembly: AssemblyFileVersion("1.0.8.0")]
+[assembly: AssemblyVersion("1.0.9.0")]
+[assembly: AssemblyFileVersion("1.0.9.0")]
