769c28fd5f267b9b854170aa76886c8ab588fbfd
TalAloni <tal.aloni.il@gmail.com>
2020-12-25 16:39:00 +0200

    Added VS2019 solution

--- HardDiskValidator/ILMerge/ILMerge.bat
+++ HardDiskValidator/ILMerge/ILMerge.bat
@@ -1,6 +1,10 @@
-set binaryPath=%CD%\..\bin\Release
-set outputPath=%CD%\..\bin\ILMerge
-IF NOT EXIST "%outputPath%" MKDIR "%outputPath%"
 IF ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles%\Microsoft\ILMerge"
 IF NOT ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles(x86)%\Microsoft\ILMerge"
-%ilmergePath%\ilmerge /ndebug /target:winexe /out:"%outputPath%\HardDiskValidator.exe" "%binaryPath%\HardDiskValidator.exe" "%binaryPath%\DiskAccessLibrary.dll" "%binaryPath%\DiskAccessLibrary.Win32.dll" "%binaryPath%\Utilities.dll"
+
+set TargetFramework=%1
+if %TargetFramework%==net20 set TargetPlaform=2.0
+if %TargetFramework%==net40 set TargetPlaform=4.0
+set binaryPath=%CD%\..\bin\Release\%TargetFramework%
+set outputPath=%CD%\..\bin\ILMerge\%TargetFramework%
+IF NOT EXIST "%outputPath%" MKDIR "%outputPath%"
+%ilmergePath%\ilmerge /targetplatform=%TargetPlaform% /ndebug /target:winexe /out:"%outputPath%\HardDiskValidator.exe" "%binaryPath%\HardDiskValidator.exe" "%binaryPath%\DiskAccessLibrary.dll" "%binaryPath%\DiskAccessLibrary.Win32.dll" "%binaryPath%\DiskAccessLibrary.FileSystems.Abstractions.dll"
