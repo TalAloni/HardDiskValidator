8f28f57be8a2471fe51a065bbf9622efb0046569
Tal Aloni <tal.aloni.il@gmail.com>
2021-06-23 20:54:25 +0300

    Incremented version to 1.1.2

--- HardDiskValidator/Properties/AssemblyInfo.cs
+++ HardDiskValidator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tal Aloni")]
 [assembly: AssemblyProduct("Hard Disk Validator")]
-[assembly: AssemblyCopyright("Copyright © Tal Aloni 2016-2019")]
+[assembly: AssemblyCopyright("Copyright © Tal Aloni 2016-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
