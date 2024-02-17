3162dd59502ae25b893118d6ed51ce0dc07ccf2d
Tal Aloni <tal.aloni.il@gmail.com>
2024-02-17 14:00:58 +0200

    Updated DiskAccessLibrary to v1.6.1

--- HardDiskValidator/ILMerge/ILMerge.bat
+++ HardDiskValidator/ILMerge/ILMerge.bat
@@ -7,4 +7,4 @@ if %TargetFramework%==net40 set TargetPlaform=4.0
 set binaryPath=%CD%\..\bin\Release\%TargetFramework%
 set outputPath=%CD%\..\bin\ILMerge\%TargetFramework%
 IF NOT EXIST "%outputPath%" MKDIR "%outputPath%"
-%ilmergePath%\ilmerge /targetplatform=%TargetPlaform% /ndebug /target:winexe /out:"%outputPath%\HardDiskValidator.exe" "%binaryPath%\HardDiskValidator.exe" "%binaryPath%\DiskAccessLibrary.dll" "%binaryPath%\DiskAccessLibrary.Win32.dll" "%binaryPath%\DiskAccessLibrary.FileSystems.Abstractions.dll"
+%ilmergePath%\ilmerge /targetplatform=%TargetPlaform% /ndebug /target:winexe /out:"%outputPath%\HardDiskValidator.exe" "%binaryPath%\HardDiskValidator.exe" "%binaryPath%\DiskAccessLibrary.dll" "%binaryPath%\DiskAccessLibrary.Win32.dll"
