841c2212616878d47fdf4cf67fbdf339988984ed
Tal Aloni <tal.aloni.il@gmail.com>
2024-02-17 13:54:41 +0200

    Remove Visual Studio 2005 support

--- HardDiskValidator/ILMerge/ILMerge.VS2005.bat
+++
@@ -1,6 +0,0 @@
-IF ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles%\Microsoft\ILMerge"
-IF NOT ["%programfiles(x86)%"]==[""] SET ilmergePath="%programfiles(x86)%\Microsoft\ILMerge"
-set binaryPath=%CD%\..\bin\Release
-set outputPath=%CD%\..\bin\ILMerge
-IF NOT EXIST "%outputPath%" MKDIR "%outputPath%"
-%ilmergePath%\ilmerge /ndebug /target:winexe /out:"%outputPath%\HardDiskValidator.exe" "%binaryPath%\HardDiskValidator.exe" "%binaryPath%\DiskAccessLibrary.dll" "%binaryPath%\DiskAccessLibrary.Win32.dll" "%binaryPath%\Utilities.dll"
