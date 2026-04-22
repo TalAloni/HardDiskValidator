a69f03121aaa01f57828f712fa188a2f95a2b49f
Tal Aloni <tal.aloni.il@gmail.com>
2026-04-22 10:02:56 +0300

    Updated Publish.bat

--- HardDiskValidator/Publish.bat
+++ HardDiskValidator/Publish.bat
@@ -1,1 +1,3 @@
+msbuild /p:TargetFramework=net20 /p:Configuration=Release
+msbuild /p:TargetFramework=net40 /p:Configuration=Release
 dotnet publish HardDiskValidator.csproj -r win7-x64 --framework netcoreapp3.1 -c Release --self-contained /p:PublishSingleFile=true
